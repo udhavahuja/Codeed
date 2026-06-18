304682f05a8cce02be4430e2dc1d8be5a0a21b41
Udhav Ahuja <udhav19ahuja@gmail.com>
2026-06-18 17:27:02 +0530

    Connect MongoDB Cloud

--- my_backend/config/db.py
+++ my_backend/config/db.py
@@ -1,9 +1,9 @@
+import os
 from pymongo import MongoClient
 
 # MongoDB connection
-MONGO_URI = "mongodb://localhost:27017/"
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(os.getenv("MONGO_URI"))
 
 # Database name
 db = client["myappdb"]
