b08b1a24b2a352d314638e399b98eb2b8e99ca54
Udhav Ahuja <udhav19ahuja@gmail.com>
2026-06-18 17:38:39 +0530

    fixed render port binding

--- my_backend/app.py
+++ my_backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 
@@ -38,5 +39,6 @@ def home():
     return "Backend API running 🚀"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
     
